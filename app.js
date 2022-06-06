ed213c65795066dfbbf44bb422726fd37eac44ac
Thomas Werner <tom.renrew@gmail.com>
2022-06-06 16:05:26 +0200

    Every Day

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.get("/", function (req, res) {
   var currentDay = today.getDay();
   var day = "";
 
+  // Logic with IF
+  /*
   if (currentDay === 0) {
     day = "Sunday";
   } else if (currentDay === 1) {
@@ -22,8 +24,39 @@ app.get("/", function (req, res) {
     day = "Thursday";
   } else if (currentDay === 5) {
     day = "Friday";
-  } else {
+  } else if (currentDay === 6) {
     day = "Saturday";
+  } else {
+    console.log("Error currentDay equals to: " + currentDay);
+  }
+*/
+
+  // Logic with Switch
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Sunday";
+      break;
+    default:
+      console.log("Error currentDay equals to: " + currentDay);
   }
   res.render("list", { kindOfDay: day });
 });
