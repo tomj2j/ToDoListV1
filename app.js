54ee69be65a54dc21b33788150e97e91b348c842
Thomas Werner <tom.renrew@gmail.com>
2022-06-06 15:57:24 +0200

    Next Step, Added Daily Welcome Text

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  req.setEncoding("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  if (currentDay === 0) {
+    day = "Sunday";
+  } else if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay === 5) {
+    day = "Friday";
+  } else {
+    day = "Saturday";
+  }
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
