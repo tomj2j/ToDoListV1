9df6501e223350fb93c90e01493bcb55b3df80f0
Thomas Werner <tom.renrew@gmail.com>
2022-06-06 19:11:20 +0200

    Added the List, Added Input, Added For Loop ESJ

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -17,57 +19,15 @@ app.get("/", function (req, res) {
   };
   var today = new Date();
   var todayFormat = today.toLocaleDateString("de-DE", options);
-  // Logic with IF
-  /*
-  if (currentDay === 0) {
-    day = "Sunday";
-  } else if (currentDay === 1) {
-    day = "Monday";
-  } else if (currentDay === 2) {
-    day = "Tuesday";
-  } else if (currentDay === 3) {
-    day = "Wednesday";
-  } else if (currentDay === 4) {
-    day = "Thursday";
-  } else if (currentDay === 5) {
-    day = "Friday";
-  } else if (currentDay === 6) {
-    day = "Saturday";
-  } else {
-    console.log("Error currentDay equals to: " + currentDay);
-  }
-*/
-
-  /*  // Logic with Switch
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Sunday";
-      break;
-    default:
-      console.log("Error currentDay equals to: " + currentDay);
-  } */
-  res.render("list", { kindOfDay: todayFormat });
+  res.render("list", { kindOfDay: todayFormat, newListItems: items });
 });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
